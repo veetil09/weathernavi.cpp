b99da8851d5fd11f583f303ec4e21e88670885aa
Veetil <sanjai.veetil@ibm.com>
2017-11-25 10:34:22 -0500

    Initial Commit

---
+++ weathernavi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  weathernavi
+//
+//  Created by Veetil on 2017-11-25.
+//  Copyright © 2017 Veetil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
