c53a1a671990e29222c8196e89fbe60719afb940
Veetil <sanjai.veetil@ibm.com>
2017-11-26 20:28:03 -0500

    All off them

---
+++ cppPlayground/cppPlayground/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  cppPlayground
+//
+//  Created by Veetil on 2017-11-26.
+//  Copyright © 2017 Veetil. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    cout << "Hello, World!\n";
+    cout << *argv << endl << endl;
+    
+    return 0;
+}
